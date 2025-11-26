c3c06f268b09dd97650c6153599bd95d595dd4c9
thenav56 <ayernavin@gmail.com>
2025-11-26 20:32:26 +0545

    feat: add sub-module-sync

---
+++ scripts/sub-module-sync.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+echo "Processing for repo: `pwd`"
+read -p "Proceed? Y to confirm: " confirm
+
+if [[ "$confirm" != "Y" ]]; then
+    echo "Aborted by user."
+    exit 1
+fi
+
+git submodule foreach 'git fetch'
+git submodule foreach 'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo main)'
